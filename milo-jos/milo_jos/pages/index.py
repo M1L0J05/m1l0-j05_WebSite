0d6f3f5f78880429a946c9001833144d01e229ec
M1L0J05 <moyo.gsxr@gmail.com>
2024-01-05 00:18:21 +0100

    Extraido index a pagina independiente

---
+++ milo-jos/milo_jos/pages/index.py
@@ -0,0 +1,26 @@
+import reflex as rx
+
+@rx.page(route='/', title='miloJosWeb')
+def index() -> rx.Component:
+    return rx.fragment(
+        rx.color_mode_button(rx.color_mode_icon(), float="right"),
+        rx.vstack(
+            rx.heading("Welcome to MiloJos_Web!", font_size="2em"),
+            rx.box("Get started by editing ",),
+            rx.link(
+                "Check out our docs!",
+                border="0.1em solid",
+                padding="0.5em",
+                border_radius="0.5em",
+                _hover={
+                    "color": rx.color_mode_cond(
+                        light="rgb(107,99,246)",
+                        dark="rgb(179, 175, 255)",
+                    )
+                },
+            ),
+            spacing="1.5em",
+            font_size="2em",
+            padding_top="10%",
+        ),
+    )

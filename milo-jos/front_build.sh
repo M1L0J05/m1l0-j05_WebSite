1164dde42d7f4c21471c9ac846b863b556bb510b
m1l0j05 <moyo.gsxr@gmail.com>
2024-02-05 20:46:08 +0100

    Añadido script para construccion del forntend para produccion

---
+++ milo-jos/front_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source .env/bin/activate
+
+pip install --upgrade pip
+
+rm -rf public
+
+reflex init
+reflex expor --frontend-only
+unzip -f frontend.zip -d public
+rm -f frontend.zip
+deactivate

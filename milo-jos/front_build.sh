8e13f4aae2fc3cd7a85e153be176976f173beacd
m1l0j05 <moyo.gsxr@gmail.com>
2024-02-05 21:01:35 +0100

    Despliegue añadiendo pagina mostrando estado en construccion v1.0.2

--- milo-jos/front_build.sh
+++ milo-jos/front_build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 source .venv/bin/activate
-
 pip install --upgrade pip
-
 rm -rf public
-
 reflex init
-reflex expor --frontend-only
-unzip -f frontend.zip -d public
+reflex export --frontend-only
+unzip  frontend.zip -d public
 rm -f frontend.zip
 deactivate

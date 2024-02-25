248be1d625556c19a844b4d7715cc4d88f39fd40
m1l0j05 <moyo.gsxr@gmail.com>
2024-02-25 19:38:02 +0100

    Corregido bug, intento de conexion con api al iniciar variable estatica

--- milo-jos/milo_jos/pages/projects.py
+++ milo-jos/milo_jos/pages/projects.py
@@ -12,113 +12,103 @@ from milo_jos.styles.colors import Colors
 with open('assets/data/projects.json') as f:
     json_data = json.load(f)  # Cargamos los datos del archivo JSON
 
-class Proyectos(rx.State):
-    proyectos:  list[dict] = json_data['proyectos']
 
-    #def get_data(self):
-    #    with open('assets/data/projects.json') as f:
-    #        json_data = json.load(f)
-    #    print(json_data['proyectos'])
-    #    self.proyectos = json_data['proyectos']
-
-
-    def card_proyecto(self, proyecto: dict):
-        return rx.card(
-            rx.inset(
-                rx.image(
-                    src=proyecto['imagen'],
-                    width='100%',
-                    margin='auto'
-                ),
-                side="top",
-                pb="current",
+def card_proyecto(proyecto: dict):
+    return rx.card(
+        rx.inset(
+            rx.image(
+                src=proyecto['imagen'],
+                width='100%',
+                margin='auto'
             ),
-            quote(
-                proyecto['nombre'],
-                text_align='center',
-                font_weight=FontWeight.BOLD.value,
-                size=Spacing.LARGE.value,
-            ),
-            rx.text(
-                '- Resumen -',
-                color=Colors.SECONDARY.value,
-                text_align='center',
-                font_size=Size.MEDIUM.value,
-                margin_top=Size.DEFAULT.value,
-                margin_bottom=Size.MICRO_SMALL.value,
-            ),
-            rx.text(
-                proyecto['resumen'],
-            ),
-            rx.text(
-                '- Tecnologías -',
-                color=Colors.SECONDARY.value,
-                text_align='center',
-                font_size=Size.MEDIUM.value,
-                margin_y=Size.MICRO_SMALL.value,
-            ),
-            rx.flex(
-                *[
-                    rx.text(
-                        class_name=f'devicon-{tech.lower()}-plain',
-                        font_size=Size.MEDIUM.value,
-                    )
-                    for tech in proyecto['tecnologias']
-                ],
-                wrap='wrap',
-                spacing=Spacing.SMALL.value,
-                justify='center',
-                align='center', 
-            ),
-            rx.text(
-                '- Inicio & Fin -',
-                color=Colors.SECONDARY.value,
-                text_align='center',
-                font_size=Size.MEDIUM.value,
-                margin_y=Size.MICRO_SMALL.value,
-            ),
-            rx.vstack(
-                rx.text(
-                    proyecto['fecha_inicio'], 
-                    text_align='center',
-                ),
-                rx.text(
-                    proyecto['fecha_fin'].upper(), 
-                    color=Colors.ACCENT.value,
-                    text_align='center',
-                ),
-                align='center',
-                justify='center'
-            ),
-
-            max_width=MID_WIDTH,
-            background_color='transparent',
-            size=Spacing.DEFAULT.value,
-            width=['90vw','90vw','45vw','45vw','33vw',],
-        )
-    
-    def render_proyecto(self ):
-        return rx.flex(
+            side="top",
+            pb="current",
+        ),
+        quote(
+            proyecto['nombre'],
+            text_align='center',
+            font_weight=FontWeight.BOLD.value,
+            size=Spacing.LARGE.value,
+        ),
+        rx.text(
+            '- Resumen -',
+            color=Colors.SECONDARY.value,
+            text_align='center',
+            font_size=Size.MEDIUM.value,
+            margin_top=Size.DEFAULT.value,
+            margin_bottom=Size.MICRO_SMALL.value,
+        ),
+        rx.text(
+            proyecto['resumen'],
+        ),
+        rx.text(
+            '- Tecnologías -',
+            color=Colors.SECONDARY.value,
+            text_align='center',
+            font_size=Size.MEDIUM.value,
+            margin_y=Size.MICRO_SMALL.value,
+        ),
+        rx.flex(
             *[
-                self.card_proyecto(
-                    proyecto
+                rx.text(
+                    class_name=f'devicon-{tech.lower()}-plain',
+                    font_size=Size.MEDIUM.value,
                 )
-                for proyecto in self.proyectos
+                for tech in proyecto['tecnologias']
             ],
+            wrap='wrap',
             spacing=Spacing.SMALL.value,
             justify='center',
-            flex_wrap='wrap',
-            margin='auto',
-        )
+            align='center', 
+        ),
+        rx.text(
+            '- Inicio & Fin -',
+            color=Colors.SECONDARY.value,
+            text_align='center',
+            font_size=Size.MEDIUM.value,
+            margin_y=Size.MICRO_SMALL.value,
+        ),
+        rx.vstack(
+            rx.text(
+                proyecto['fecha_inicio'], 
+                text_align='center',
+            ),
+            rx.text(
+                proyecto['fecha_fin'].upper(), 
+                color=Colors.ACCENT.value,
+                text_align='center',
+            ),
+            align='center',
+            justify='center'
+        ),
+
+        max_width=MID_WIDTH,
+        background_color='transparent',
+        size=Spacing.DEFAULT.value,
+        width=['90vw','90vw','45vw','45vw','33vw',],
+    )
+
+def render_proyecto(proyectos=json_data['proyectos'] ):
+    return rx.flex(
+        *[
+            card_proyecto(
+                proyecto
+            )
+            for proyecto in proyectos
+        ],
+        spacing=Spacing.SMALL.value,
+        justify='center',
+        flex_wrap='wrap',
+        margin='auto',
+    )
 
 
-@template(route='/projects', title='m1l0_j05 WebSite', )
+@template(route='/projects', title='m1l0_j05 WebSite' )
 def projects( ) -> rx.Component:
-    proyectos = Proyectos()
     return rx.fragment(
         rx.vstack(
             rx.spacer(),
-            proyectos.render_proyecto(),
+            render_proyecto(),
             width='95%',
             min_width=MIN_WIDTH,
             margin_top=Size.BIG.value,
